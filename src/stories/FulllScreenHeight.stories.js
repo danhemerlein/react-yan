04adef451af17fef76dc314c5f1b993a26510554
Dan Hemerlein <danhemerlein@gmail.com>
2022-03-26 20:19:19 -0400

    add a control to the rull screen componennt

--- src/stories/FulllScreenHeight.stories.js
+++ src/stories/FulllScreenHeight.stories.js
@@ -2,34 +2,40 @@ import styled from 'styled-components';
 import { FullScreenHeight } from '../components/FullScreenHeight';
 import { P } from '../components/Typography';
 
-const SFullScreenHeight = styled(FullScreenHeight)`
+const StoryFullScreenHeight = styled(FullScreenHeight)`
   border: 5px solid black;
 `;
 
 export default {
   title: 'Fulll Screen Height',
   argTypes: {
-    unsetBreakpoint: {
-      options: ['none', 'mobile', 'tablet', 'desktop-max'],
-      control: { type: 'radio' }
-    },
     offset: {
       type: 'number',
-      defaultValue: 0
-    }
-  }
+      defaultValue: 0,
+    },
+    unsetBreakpoint: {
+      options: ['none', 'mobile', 'tablet', 'desktop-max'],
+      control: { type: 'radio' },
+      defaultValue: 'none',
+    },
+    direction: {
+      options: ['column', 'row'],
+      control: { type: 'radio' },
+      defaultValue: 'row',
+    },
+  },
 };
 
-const Template = ({ unsetBreakpoint, offset }) => {
+const Template = ({ unsetBreakpoint, offset, direction }) => {
   return (
-    <SFullScreenHeight
+    <StoryFullScreenHeight
       unsetBreakpoint={unsetBreakpoint}
       offset={offset}
-      direction="column"
+      direction={direction}
     >
       <P>content</P>
       <P>content</P>
-    </SFullScreenHeight>
+    </StoryFullScreenHeight>
   );
 };
 
